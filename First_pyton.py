35868311538a52626c7b5b1c6f1f27ff74144219
Рома <top77972@gmail.com>
2022-08-01 22:55:25 +0300

    init

---
+++ First_pyton.py
@@ -0,0 +1,5 @@
+from __future__ import braces  #Це модуль використовується для надання функцій з майбутніх версій Python.
+
+import this  #Виводиться Zen of Python, типу це філософія програмування на пайтон
+import antigravity  #Цей модуль відкриває в браузері комікс XKCD
+import __hello__  #Цей модуль виводить в консоль Hello World!
