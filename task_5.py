1a5b318b8e05d0b9520201c700cda6d715c73a87
1r0m <108801167+1r0m@users.noreply.github.com>
2022-08-06 13:08:58 +0300

    Lessons_2

---
+++ task_5.py
@@ -0,0 +1,7 @@
+number = int(input("Вкажіть число для перевірки : "))
+if number > 0 :
+  print("Число додатнє")
+elif number < 0 :
+  print("Число від'ємне")
+if number == 0 :
+  print("Число рівнє нулю")
