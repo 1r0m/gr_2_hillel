ec77b5d5e1bd36c9325c0bc1597322057e614cea
1r0m <108801167+1r0m@users.noreply.github.com>
2022-08-28 11:20:30 +0300

    Update task_2.py

--- lesson_5/task_2.py
+++ lesson_5/task_2.py
@@ -1,42 +1,44 @@
 import requests
 import pprint
 
-def number():
-	num = int(input("Вкажіть число до 100:"))
+url = 'https://script.google.com/macros/s/AKfycbymoe4FLilr2JMNUshhMEYDqDlFqjGo06Y8e2IqQ-q-dNVDQjdS77Qz_QVIz4iRRIAa/exec'
 
-	while num <= 100:
-		if num <= 100 :
-			print("Повторіть введене число")
-		if num < 0:
-			print("Число менше 0 введіть число більше")
-		elif num == 0:
-			print("Число дорівнює 0")
-			continue
-	print("Ви ввели значення більше 100")
 
-def URL():
-	url = 'https://script.google.com/macros/s/AKfycbymoe4FLilr2JMNUshhMEYDqDlFqjGo06Y8e2IqQ-q-dNVDQjdS77Qz_QVIz4iRRIAa/exec'
+def get_data_api_url(url):
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-	response = requests.get(url)
-	data = response.json()
-	pprint.pprint(data)
 
-def len_list():
-	text_message = input("Напишіть повідомлення менше за 150 символів:")
-	text = text_message[:150]
-	print(len(text))
-	new_text = '...'
-	result = text[:-3] + new_text
-	print(result)
-	
+def check_number_in_range(number, low=0, hight=100):
+    if number >= low and number <= hight:
+        return True
+    return False
+
+
+def message_cater(string, lengs=150):
+	message_text = input("")
+
+	text = message_text[:lengs]
+	lengs_text = len(message_text)
+
+
+	if lengs_text > lengs:
+		new_text = '...'
+		result = message_text[:-3] + new_text
+		print(result)
+	else:
+		print(message_text)
+
+
+def write_file_in_document():
 	with open(r"E:\\python.txt", 'w') as file:
 		file.write(result)
 		
 
-	
-	
 
 if __name__ == '__main__':
-	number()
-	URL()
-	len_list()
+	get_data_api_url(url=url)
+	check_number_in_range()
+	message_cater()
+	write_file_in_document()
